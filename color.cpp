586f7dbe16548ba055833be5a06ccf7fdce01fa3
Kan Sovichai <ksovichai08@gmail.com>
2025-03-01 20:51:04 +0700

    Color code for C++

---
+++ color.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\033[031m" << "This color is red" << "\033[0m" << endl;
+    cout << "\033[032m" << "This color is green" << "\033[0m" << endl;
+    cout << "\033[033m" << "This color is yellow" << "\033[0m" << endl;
+    cout << "\033[034m" << "This color is blue " << "\033[0m" << endl;
+    cout << "\033[035m" << "This color is pink" << "\033[0m" << endl;
+    cout << "\033[036m" << "This color is cyan " << "\033[0m" << endl;
+    cout << "\033[037m" << "This color is " << "\033[0m" << endl;
+    cout << "\033[041m" << "This color is red" << "\033[0m" << endl;
+    cout << "\033[042m" << "This color is green" << "\033[0m" << endl;
+    cout << "\033[043m" << "This color is yellow" << "\033[0m" << endl;
+    cout << "\033[044m" << "This color is blue" << "\033[0m" << endl;
+    cout << "\033[045m" << "This color is pink" << "\033[0m" << endl;
+    cout << "\033[046m" << "This color is cyan " << "\033[0m" << endl;
+    return 0;
+}
